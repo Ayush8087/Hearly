34ed7616436f6519fc045787b58fbdb1a0f3e994
Ayush Ravi <ayushravi8087@gmail.com>
2025-09-09 17:23:29 +0530

    fixing render deploy issue

--- app/api/playlists/[id]/songs/[songId]/route.js
+++ app/api/playlists/[id]/songs/[songId]/route.js
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../../auth/[...nextauth]/options";
+import { authOptions } from "../../../../auth/[...nextauth]/options";
 
 async function requireSession() {
   const session = await getServerSession(authOptions);
