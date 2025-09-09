eda9bba7090be32da1aaaf566860eade68fab008
Ayush Ravi <ayushravi8087@gmail.com>
2025-09-10 01:10:19 +0530

    fixing prior error

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'https://hearly.onrender.com',
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://saavn.dev/api/',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://hearly-s1u6.onrender.com/',
   },
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'bcrypt'],
