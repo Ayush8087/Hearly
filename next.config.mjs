ba09d2efbe7afe26c7e40f12de23ae9331d8ba0d
Ayush Ravi <ayushravi8087@gmail.com>
2025-09-11 16:01:30 +0530

    playlist song saving issue 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'https://hearly.onrender.com',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || (process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://hearly.onrender.com'),
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://hearly-s1u6.onrender.com/',
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || 'fallback-secret-key-for-development',
   },
